bfc33b475f0c3017eb95d64f4b94d9d8e6cb3d93
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-11-20 17:11:18 +0000

    ...

---
+++ Assets/_Scripts/LoadAssetBundle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.oculus.demo/main.51.com.flowmeditation.flowvr.obb");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
