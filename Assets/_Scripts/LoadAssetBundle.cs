e4b2b74c5d7c3d2d07c499db788f9a87360e5de8
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-11-21 16:43:24 +0000

    Still trying to get this build method to work

--- Assets/_Scripts/LoadAssetBundle.cs
+++ Assets/_Scripts/LoadAssetBundle.cs
@@ -7,7 +7,7 @@ public class LoadAssetBundle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.oculus.demo/main.51.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.oculus.demo/main.55.com.flowmeditation.flowvr.obb");
 	}
 
     // Update is called once per frame
