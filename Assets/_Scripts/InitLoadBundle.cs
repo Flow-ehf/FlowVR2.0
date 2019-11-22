348a22c119f2efc3972513fc669c5581fed4ab47
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-11-22 17:21:55 +0000

    This is driving me insane

---
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class InitLoadBundle 
+{
+//	static readonly int bundleVersionCode = 69;
+
+	[RuntimeInitializeOnLoadMethod]
+    static void Init()
+	{
+//#if UNITY_ANDROID
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.69.com.flowmeditation.flowvr.obb");
+//#endif
+	}
+}
