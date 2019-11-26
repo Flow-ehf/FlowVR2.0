be6abb332d8a2bebde2ee116226c8ccc0450286e
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-11-26 16:00:00 +0000

    Testing building dlc

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public static class InitLoadBundle 
 {
-//	static readonly int bundleVersionCode = 69;
+//	static readonly int bundleVersionCode = 72;
 
 	[RuntimeInitializeOnLoadMethod]
     static void Init()
 	{
 #if UNITY_ANDROID
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.70.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.72.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.72.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.72.com.flowmeditation.flowvr.obb");
 #endif
 	}
 }
