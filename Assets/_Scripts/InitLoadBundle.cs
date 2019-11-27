244d381be7ffd474d0ee6b55801421e2ee040663
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-11-27 13:07:02 +0000

    Done testing build with dlc and it works

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -11,8 +11,8 @@ public static class InitLoadBundle
 	{
 #if UNITY_ANDROID
 		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.72.com.flowmeditation.flowvr.obb");
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.72.com.flowmeditation.flowvr.obb");
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.72.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.72.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.72.com.flowmeditation.flowvr.obb");
 #endif
 	}
 }
