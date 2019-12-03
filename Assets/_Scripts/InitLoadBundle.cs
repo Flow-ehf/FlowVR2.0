e3fae226c16d3d4c9c5b7f3b6b84cd84dfc8e6c2
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-12-03 12:46:48 +0000

    No splash screen

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public static class InitLoadBundle 
 {
-//	static readonly int bundleVersionCode = 72;
+//	static readonly int bundleVersionCode = 74;
 
 	[RuntimeInitializeOnLoadMethod]
     static void Init()
 	{
 #if UNITY_ANDROID
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.72.com.flowmeditation.flowvr.obb");
-		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.72.com.flowmeditation.flowvr.obb");
-		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.72.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.74.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.74.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.74.com.flowmeditation.flowvr.obb");
 #endif
 	}
 }
