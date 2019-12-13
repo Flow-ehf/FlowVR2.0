f11a64783b6c6dfed2b27ad3c886e3fb8b57425d
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2019-12-13 14:01:50 +0000

    tiny fixes to UI and volume

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public static class InitLoadBundle 
 {
-//	static readonly int bundleVersionCode = 74;
+//	static readonly int bundleVersionCode = 83;
 
 	[RuntimeInitializeOnLoadMethod]
     static void Init()
 	{
 #if UNITY_ANDROID
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.74.com.flowmeditation.flowvr.obb");
-		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.74.com.flowmeditation.flowvr.obb");
-		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.74.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.83.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.83.com.flowmeditation.flowvr.obb");
+		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.83.com.flowmeditation.flowvr.obb");
 #endif
 	}
 }
