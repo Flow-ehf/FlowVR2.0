02fb7b5cca2516b9e460e51e526243bd1a7faa61
DESKTOP-O02OFE5\gnusi <stefansaegull@gmail.com>
2020-08-10 15:40:23 +0000

    Added manifest and updated some settings

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public static class InitLoadBundle 
 {
-//	static readonly int bundleVersionCode = 106;
+//	static readonly int bundleVersionCode = 108;
 
 	[RuntimeInitializeOnLoadMethod]
     static void Init()
 	{
 #if UNITY_ANDROID && !UNITY_EDITOR
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.106.com.flowmeditation.flowvr.obb");
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets1.106.com.flowmeditation.flowvr.obb");
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack2 - assets2.106.com.flowmeditation.flowvr.obb");
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack3 - assets3.106.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.108.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets1.108.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack2 - assets2.108.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack3 - assets3.108.com.flowmeditation.flowvr.obb");
 #endif
 	}
 }
