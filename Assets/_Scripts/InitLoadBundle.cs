2f13c75b52008b3dfcd4eaa61445a2f8d5b1c31b
jodern <jo_harald@hotmail.com>
2020-08-10 21:36:14 +0200

    Assetbundles loaded async

--- Assets/_Scripts/InitLoadBundle.cs
+++ Assets/_Scripts/InitLoadBundle.cs
@@ -10,7 +10,7 @@ public static class InitLoadBundle
     static void Init()
 	{
 #if UNITY_ANDROID && !UNITY_EDITOR
-		AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/main.88.com.flowmeditation.flowvr.obb");
+		AssetBundle.LoadFromFileAsync("/sdcard/Android/obb/com.flowmeditation.flowvr/main.88.com.flowmeditation.flowvr.obb");
 		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - assets.88.com.flowmeditation.flowvr.obb");
 		//AssetBundle.LoadFromFile("/sdcard/Android/obb/com.flowmeditation.flowvr/pack1 - scenes.88.com.flowmeditation.flowvr.obb");
 #endif
